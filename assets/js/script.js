ab99fd90fb946123c6cfbbff07801960bc1b2465
Matstaps <matstaplehurst@yahoo.co.uk>
2023-01-05 15:29:52 +0000

    Amend code to tidy

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,7 @@ function runGame(gameType) {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "division") {
         displayDivisionQuestion(num1, num2);
-    }else {
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -92,7 +92,7 @@ function calculateCorrectAnswer() {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "/") {
         return [Math.floor(operand1 / operand2), "division"];
-    }else {
+    } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
